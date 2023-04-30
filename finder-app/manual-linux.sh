ce1537d456e505253fff833e28c4eec2e5cbc091
ssari-memory <sercan.sari@std.yeditepe.edu.tr>
2023-04-30 09:51:28 -0700

    implemented manual build

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,10 +34,19 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    
+    echo "TODO: Add your kernel build steps here"
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
 fi
 
 echo "Adding the Image in outdir"
+
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}
+
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +56,14 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+echo "TODO: Create necessary base directories / creating a folder tree"
+cd "$OUTDIR"
+mkdir -p rootfs
+cd rootfs
+
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +71,60 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    echo "TODO:  Configure busybox"
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+echo "TODO: Make and install busybox"
+
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=$OUTDIR/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+echo "TODO: Add library dependencies to rootfs"
 
-# TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+TOOLCHAIN=$(aarch64-none-linux-gnu-gcc -print-sysroot)
+cp -r ${TOOLCHAIN}/lib/* lib/
+cp -r ${TOOLCHAIN}/lib64/* lib64/
 
-# TODO: Clean and build the writer utility
+echo "TODO: Make device nodes"
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
-# TODO: Chown the root directory
 
-# TODO: Create initramfs.cpio.gz
+echo "TODO: Clean and build the writer utility"
+
+cd "${FINDER_APP_DIR}"
+make clean
+make CROSS_COMPILE=aarch64-none-linux-gnu-
+
+echo "TODO: Copy the finder related scripts and executables to the /home directory "
+echo "on the target rootfs"
+
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh ${OUTDIR}/rootfs/home/
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp -r conf/ ${OUTDIR}/rootfs/home/
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+
+echo "TODO: Chown the root directory"
+
+cd ${OUTDIR}/rootfs
+sudo chown -R root ${OUTDIR}/rootfs/
+
+echo "TODO: Create initramfs.cpio.gz"
+
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ..
+gzip -f ${OUTDIR}/initramfs.cpio
+
+echo "SUCCESS"
